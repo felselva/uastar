061ac09a37885e818007471840ad72872e321ea5
Felipe Ferreira da Silva <felselva@protonmail.com>
2019-01-01 23:13:11 -0300

    Add compilation command in test

--- test.c
+++ test.c
@@ -17,7 +17,8 @@ the following restrictions:
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 
-
+Compiling with GCC:
+  gcc test.c uastar.c -o test
 */
 
 #include <stdlib.h>
